24f5b02934f17033b8444013fcc6ad165e137e65
Abhijit Jayaraman <abhijayaraman12@gmail.com>
2026-08-20 23:31:45 +0800

    added tasks

--- src/main/java/Gunna.java
+++ src/main/java/Gunna.java
@@ -13,8 +13,8 @@ public class Gunna {
 
         System.out.println(delimiter);
         System.out.println(banner);
-        System.out.println("Hello! I'm GUNNA.");
-        System.out.println("What can I do for you?");
+        System.out.println("     Hello! I'm GUNNA.");
+        System.out.println("     What can I do for you?");
         System.out.println(delimiter);
 
         // Array to store tasks (max 100)
@@ -29,14 +29,14 @@ public class Gunna {
 
             if (command.equals("bye")) {
                 System.out.println(delimiter);
-                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("     Bye. Hope to see you again soon!");
                 System.out.println(delimiter);
                 break;
             } else if (command.equals("list")) {
                 System.out.println(delimiter);
-                System.out.println("Here are the tasks in your list:");
+                System.out.println("     Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + "." + tasks[i]);
+                    System.out.println("     " + (i + 1) + "." + tasks[i]);
                 }
                 System.out.println(delimiter);
             } else if (command.startsWith("mark ")) {
@@ -47,8 +47,8 @@ public class Gunna {
                 if (taskIndex >= 0 && taskIndex < taskCount) {
                     tasks[taskIndex].markAsDone();
                     System.out.println(delimiter);
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println("  " + tasks[taskIndex]);
+                    System.out.println("     Nice! I've marked this task as done:");
+                    System.out.println("       " + tasks[taskIndex]);
                     System.out.println(delimiter);
                 }
             } else if (command.startsWith("unmark ")) {
@@ -59,17 +59,68 @@ public class Gunna {
                 if (taskIndex >= 0 && taskIndex < taskCount) {
                     tasks[taskIndex].markAsNotDone();
                     System.out.println(delimiter);
-                    System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println("  " + tasks[taskIndex]);
+                    System.out.println("     OK, I've marked this task as not done yet:");
+                    System.out.println("       " + tasks[taskIndex]);
                     System.out.println(delimiter);
                 }
-            } else {
-                // Add task to array
-                tasks[taskCount] = new Task(command);
+            } else if (command.startsWith("todo ")) {
+                // Parse todo command
+                String description = command.substring(5);
+                tasks[taskCount] = new Todo(description);
                 taskCount++;
 
                 System.out.println(delimiter);
-                System.out.println(" added: " + command);
+                System.out.println("     Got it. I've added this task:");
+                System.out.println("       " + tasks[taskCount - 1]);
+                System.out.println("     Now you have " + taskCount + " tasks in the list.");
+                System.out.println(delimiter);
+            } else if (command.startsWith("deadline ")) {
+                // Parse deadline command: deadline <description> /by <time>
+                String remaining = command.substring(9);
+                int byIndex = remaining.indexOf(" /by ");
+
+                if (byIndex == -1) {
+                    System.out.println(delimiter);
+                    System.out.println("     Please use the format: deadline <description> /by <time>");
+                    System.out.println(delimiter);
+                } else {
+                    String description = remaining.substring(0, byIndex);
+                    String by = remaining.substring(byIndex + 5);
+                    tasks[taskCount] = new Deadline(description, by);
+                    taskCount++;
+
+                    System.out.println(delimiter);
+                    System.out.println("     Got it. I've added this task:");
+                    System.out.println("       " + tasks[taskCount - 1]);
+                    System.out.println("     Now you have " + taskCount + " tasks in the list.");
+                    System.out.println(delimiter);
+                }
+            } else if (command.startsWith("event ")) {
+                // Parse event command: event <description> /from <time> /to <time>
+                String remaining = command.substring(6);
+                int fromIndex = remaining.indexOf(" /from ");
+                int toIndex = remaining.indexOf(" /to ");
+
+                if (fromIndex == -1 || toIndex == -1) {
+                    System.out.println(delimiter);
+                    System.out.println("     Please use the format: event <description> /from <time> /to <time>");
+                    System.out.println(delimiter);
+                } else {
+                    String description = remaining.substring(0, fromIndex);
+                    String from = remaining.substring(fromIndex + 7, toIndex);
+                    String to = remaining.substring(toIndex + 5);
+                    tasks[taskCount] = new Event(description, from, to);
+                    taskCount++;
+
+                    System.out.println(delimiter);
+                    System.out.println("     Got it. I've added this task:");
+                    System.out.println("       " + tasks[taskCount - 1]);
+                    System.out.println("     Now you have " + taskCount + " tasks in the list.");
+                    System.out.println(delimiter);
+                }
+            } else {
+                System.out.println(delimiter);
+                System.out.println("     I don't understand that command.");
                 System.out.println(delimiter);
             }
         }
