6d1e77eb89ef9bad1a7c9c8b0c921254f12deece
Abhijit Jayaraman <abhijayaraman12@gmail.com>
2026-08-20 21:52:12 +0800

    Changed name of chatbot as well as updating intial code to greet user

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# GUNNA User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Gunna.java
@@ -0,0 +1,20 @@
+public class Gunna {
+    public static void main(String[] args) {
+        String banner =
+                "    ____                         \n"
+                + " / ___|_   _ _ __  _ __   __ _ \n"
+                + "| |  _| | | | '_ \\| '_ \\ / _` |\n"
+                + "| |_| | |_| | | | | | | | (_| |\n"
+                + " \\____|\\__,_|_| |_|_| |_|\\__,_|\n";
+
+        String delimiter = "____________________________________________________________";
+
+        System.out.println(delimiter);
+        System.out.println(banner);
+        System.out.println("Hello! I'm GUNNA.");
+        System.out.println("What can I do for you?");
+        System.out.println(delimiter);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(delimiter);
+    }
+}
